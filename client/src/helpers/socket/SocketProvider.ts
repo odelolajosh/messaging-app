52613f8ea0c7016de565fef38d38a2bacf34f937
odelolajosh <oladapoodelola@gmail.com>
2021-11-27 07:01:05 +0100

    feature/Set remote API

--- client/src/helpers/socket/SocketProvider.ts
+++ client/src/helpers/socket/SocketProvider.ts
@@ -3,11 +3,13 @@ import io from 'socket.io-client';
 class SocketProvider {
     socket: SocketIOClient.Socket | null = null;
 
+    SOCKET_URL = process.env.NODE_ENV === 'development' 
+        ? "http://localhost:4001"
+        : "https://my-chat-app-server-0707.herokuapp.com";
+
     createSocketConnection(id: string) {
-        const url = 'http://localhost:4001'
-        // const url = 'https://my-chat-app-server-0707.herokuapp.com'
         try {
-            this.socket = io.connect(url, { query: `userId=${id}` });
+            this.socket = io.connect(this.SOCKET_URL, { query: `userId=${id}` });
         } catch(error) {
             throw new Error('Something went wrong. We can\'t connect to server')
         }
